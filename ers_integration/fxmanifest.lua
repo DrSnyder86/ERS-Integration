b2a420797352c0cab068455409721a3cf2b7e4f0
DrSnyder86 <drsnyder86@gmail.com>
2025-11-13 08:46:15 -0500

    Refine fxmanifest.lua with improved description
    Updated the description for clarity and commented out unused scripts.

--- ers_integration/fxmanifest.lua
+++ ers_integration/fxmanifest.lua
@@ -2,16 +2,18 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'DrSnyder'
-description 'Qbox ERS Integration'
+description 'ERS Integration for Qb and Qbox using Ps-dispatch and Radial Menu'
 version '1.0'
 
 
 client_scripts {
     'client.lua',
+    --'tablet.lua'
 }
 
 server_scripts {
-    '@qb-core/shared/locale.lua',    
+    '@qb-core/shared/locale.lua', 
+    --'@oxmysql/lib/MySQL.lua',      
     'server.lua'
 }
 
@@ -19,7 +21,6 @@ server_scripts {
 dependencies {
     'qb-core',
     'ps-dispatch',
+    -- 'ps-mdt',
     'ox_lib'
 }
-
-
