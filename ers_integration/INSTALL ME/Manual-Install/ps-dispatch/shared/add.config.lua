3598b223e43b0ea4aec59ff428a589ab71b8179e
DrSnyder86 <drsnyder86@gmail.com>
2025-11-13 08:48:30 -0500

    Update blip configurations in add.config.lua

--- ers_integration/INSTALL ME/Manual-Install/ps-dispatch/shared/add.config.lua
+++ ers_integration/INSTALL ME/Manual-Install/ps-dispatch/shared/add.config.lua
@@ -1,41 +1,41 @@
 --add to ps-dispatch/shared/config.lua in Config.Blips section
 
     ['codefour'] = {
-        --radius = 0,
-        --sprite = 126,
-        --color = 3,
-        --scale = 1.5,
-        --length = 2,
+        radius = 0,
+        sprite = 126,
+        color = 3,
+        scale = 0.5,
+        length = 0.1,
         sound = 'robberysound',
-        --offset = false,
-        --flash = false
+        offset = false,
+        flash = false
     },
     ['onscene'] = {
         radius = 0,
         sprite = 161,
         color = 3,
         scale = 1.5,
-        length = 2,
+        length = 1,
         sound = 'robberysound',
         offset = false,
         flash = false
     },
     ['enroute'] = {
-        --radius = 0,
-        --sprite = 126,
-        --color = 3,
-        --scale = 1.5,
-        --length = 2,
+        radius = 0,
+        sprite = 126,
+        color = 3,
+        scale = 0.5,
+        length = 0.1,
         sound = 'robberysound',
-        --offset = false,
-        --flash = false
+        offset = false,
+        flash = false
     },
     ['trafficstop'] = {
         radius = 15.0,
         sprite = 42,
         color = 1,
-        scale = 1.0,
-        length = 2,
+        scale = 0.5,
+        length = 1,
         sound = 'robberysound',
         offset = false,
         flash = false
@@ -44,7 +44,7 @@
         radius = 25.0,
         sprite = 833,
         color = 1,
-        scale = 1.5,
+        scale = 0.5,
         length = 2,
         sound = 'ringing',
         offset = false,
