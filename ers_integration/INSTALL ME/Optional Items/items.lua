df4f6b43a7cbb3850bad0a450fa9f58cce0ef36e
DrSnyder86 <drsnyder86@gmail.com>
2025-11-15 15:22:20 -0500

    Add items for ox_inventory and qb-inventory

--- ers_integration/INSTALL ME/Optional Items/items.lua
+++ ers_integration/INSTALL ME/Optional Items/items.lua
@@ -1,4 +1,5 @@
 -- ox_inventory
+-- ox_inventory/data/items.lua
 
     ['mdt_tablet'] = {
 		label = 'MDT Tablet',
@@ -32,3 +33,52 @@
             event = 'wk:openRemote'
         }
     },
+
+
+-- qb-inventory
+-- qb-core/shared/items.lua
+
+mdt_tablet = {
+    name = 'mdt_tablet',
+    label = 'MDT Tablet',
+    weight = 500,
+    type = 'item',
+    image = 'mdt_tablet.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    description = 'A police-issued tablet to access the MDT system',
+    client = {
+        event = 'custom:client:useMDTTablet'
+    }
+},
+
+broom = {
+    name = 'broom',
+    label = 'Broom',
+    weight = 100,
+    type = 'item',
+    image = 'broom.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    description = 'Clean your scenes',
+    client = {
+        event = 'custom:broom'
+    }
+},
+
+wraithradar = {
+    name = 'wraithradar',
+    label = 'Wraith Radar Remote',
+    weight = 100,
+    type = 'item',
+    image = 'wraithradar.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    description = 'Remote for Police Radar Control',
+    client = {
+        event = 'wk:openRemote'
+    }
+},
