563d4d9aa1a1bcc1abaf7bb2a31fcc10e9b865ca
DrSnyder86 <drsnyder86@gmail.com>
2025-11-17 14:31:15 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- INSTALL ME/Replace-Install/qb-radialmenu/config.lua
+++ INSTALL ME/Replace-Install/qb-radialmenu/config.lua
@@ -16,10 +16,10 @@ Config.MenuItems = {
             title = 'ERS Duty',
             icon = 'clipboard-list',
             items = {
-                { id = 'toggle_police', title = 'Toggle Police Duty', icon = 'user-shield', type = 'server', event = 'ers:server:TogglePoliceShift', shouldClose = true },
-                { id = 'toggle_ambulance', title = 'Toggle Ambulance Duty', icon = 'briefcase-medical', type = 'server', event = 'ers:server:ToggleAmbulanceShift', shouldClose = true },
-                { id = 'toggle_fire', title = 'Toggle Fire Duty', icon = 'fire-extinguisher', type = 'server', event = 'ers:server:ToggleFireShift', shouldClose = true },
-                { id = 'toggle_tow', title = 'Toggle Tow Duty', icon = 'truck', type = 'server', event = 'ers:server:ToggleTowShift', shouldClose = true },
+                { id = 'togglepolice', title = 'Toggle Police Duty', icon = 'user-shield', type = 'server', event = 'ers:server:TogglePoliceShift', shouldClose = true },
+                { id = 'toggleambulance', title = 'Toggle Ambulance Duty', icon = 'briefcase-medical', type = 'server', event = 'ers:server:ToggleAmbulanceShift', shouldClose = true },
+                { id = 'togglefire', title = 'Toggle Fire Duty', icon = 'fire-extinguisher', type = 'server', event = 'ers:server:ToggleFireShift', shouldClose = true },
+                { id = 'toggletow', title = 'Toggle Tow Duty', icon = 'truck', type = 'server', event = 'ers:server:ToggleTowShift', shouldClose = true },
             }
         },
         {
@@ -27,10 +27,10 @@ Config.MenuItems = {
             title = 'ERS Utilities',
             icon = 'list-check',
             items = {
-                { id = 'request_callout', title = 'Request 911 Call', icon = 'bell', type = 'client', event = 'callout:request', shouldClose = true },
-                { id = 'toggle_callouts', title = 'Toggle 911 Dispatch', icon = 'clipboard-list', type = 'client', event = 'callouts:toggle', shouldClose = true },
+                { id = 'requestcallout', title = 'Request 911 Call', icon = 'bell', type = 'client', event = 'callout:request', shouldClose = true },
+                { id = 'togglecallouts', title = 'Toggle 911 Dispatch', icon = 'clipboard-list', type = 'client', event = 'callouts:toggle', shouldClose = true },
                 { id = 'wraith', title = 'Wraith Radar', icon = 'car-side', type = 'client', event = 'wk:openRemote', shouldClose = true },
-                { id = 'mdt_toggle', title = 'MDT Tablet', icon = 'tablet-alt', type = 'client', event = 'mdt:toggle', shouldClose = true },
+                { id = 'mdttoggle', title = 'MDT Tablet', icon = 'clipboard-list', type = 'client', event = 'mdt:toggle', shouldClose = true },
                 { id = 'speedzone', title = 'Traffic Control', icon = 'triangle-exclamation', type = 'client', event = 'custom:speedzone', shouldClose = true },
             }
         },
@@ -39,9 +39,9 @@ Config.MenuItems = {
             title = 'Cancel Requests',
             icon = 'triangle-exclamation',
             items = {
-                { id = 'cancel_ambulance', title = 'Cancel Ambulance', icon = 'ambulance', type = 'client', event = 'call:cancelambulance', shouldClose = true },
+                { id = 'cancel_ambulance', title = 'Cancel Ambulance', icon = 'briefcase-medical', type = 'client', event = 'call:cancelambulance', shouldClose = true },
                 { id = 'cancel_fire', title = 'Cancel Fire Unit', icon = 'truck', type = 'client', event = 'call:cancelfire', shouldClose = true },
-                { id = 'cancel_police', title = 'Cancel Police', icon = 'shield-alt', type = 'client', event = 'call:cancelpolice', shouldClose = true },
+                { id = 'cancel_police', title = 'Cancel Police', icon = 'user-shield', type = 'client', event = 'call:cancelpolice', shouldClose = true },
                 { id = 'cancel_coroner', title = 'Cancel Coroner', icon = 'skull-crossbones', type = 'client', event = 'call:cancelcoroner', shouldClose = true },
                 { id = 'cancel_taxi', title = 'Cancel Taxi', icon = 'taxi', type = 'client', event = 'call:canceltaxi', shouldClose = true },
                 { id = 'cancel_tow', title = 'Cancel Tow', icon = 'truck', type = 'client', event = 'call:canceltow', shouldClose = true },
@@ -55,15 +55,15 @@ Config.MenuItems = {
             title = 'ERS Services',
             icon = 'clipboard-list',
             items = {
-                { id = 'request_ambulance', title = 'Ambulance', icon = 'ambulance', type = 'client', event = 'call:ambulance', shouldClose = true },
-                { id = 'request_police', title = 'PD Transport', icon = 'shield-alt', type = 'client', event = 'call:police', shouldClose = true },
-                { id = 'request_tow', title = 'Tow', icon = 'truck', type = 'client', event = 'call:tow', shouldClose = true },
+                { id = 'requestambulance', title = 'Ambulance', icon = 'briefcase-medical', type = 'client', event = 'call:ambulance', shouldClose = true },
+                { id = 'requestpolice', title = 'PD Transport', icon = 'user-shield', type = 'client', event = 'call:police', shouldClose = true },
+                { id = 'requesttow', title = 'Tow', icon = 'truck', type = 'client', event = 'call:tow', shouldClose = true },
                 { id = 'requestfire', title = 'Fire Unit', icon = 'truck', type = 'client', event = 'custom:requestfire', shouldClose = true },
-                { id = 'request_coroner', title = 'Coroner', icon = 'skull-crossbones', type = 'client', event = 'call:coroner', shouldClose = true },
-                { id = 'request_mechanic', title = 'Mechanic', icon = 'wrench', type = 'client', event = 'call:mechanic', shouldClose = true },
-                { id = 'request_roadservice', title = 'Road Service', icon = 'truck', type = 'client', event = 'call:roadservice', shouldClose = true },
-                { id = 'request_taxi', title = 'Taxi', icon = 'taxi', type = 'client', event = 'call:taxi', shouldClose = true },
-                { id = 'request_animal_rescue', title = 'Animal Rescue', icon = 'paw', type = 'client', event = 'call:animalrescue', shouldClose = true },
+                { id = 'requestcoroner', title = 'Coroner', icon = 'skull-crossbones', type = 'client', event = 'call:coroner', shouldClose = true },
+                { id = 'requestmechanic', title = 'Mechanic', icon = 'wrench', type = 'client', event = 'call:mechanic', shouldClose = true },
+                { id = 'requestroadservice', title = 'Road Service', icon = 'truck', type = 'client', event = 'call:roadservice', shouldClose = true },
+                { id = 'requesttaxi', title = 'Taxi', icon = 'taxi', type = 'client', event = 'call:taxi', shouldClose = true },
+                { id = 'requestanimalrescue', title = 'Animal Rescue', icon = 'paw', type = 'client', event = 'call:animalrescue', shouldClose = true },
             }
         },
         {
@@ -972,4 +972,5 @@ Config.ExtraCommands = {
         OffsetX = -0.12,
         OffsetY = 0.2
     }
+
 }
