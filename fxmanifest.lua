0f4be7c17656f25f0e5a452aa04ddaf7ad380855
DrSnyder86 <drsnyder86@gmail.com>
2025-12-02 09:28:11 -0500

    Update fxmanifest.lua for version 1.4
    Updated version number and added repository and license information.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,26 +1,32 @@
-fx_version 'cerulean'
-game 'gta5'
 
-author 'DrSnyder'
+        -- ██████╗ ██████╗ ███████╗███╗   ██╗██╗   ██╗██████╗ ███████╗██████╗ 
+        -- ██╔══██╗██╔══██╗██╔════╝████╗  ██║╚██╗ ██╔╝██╔══██╗██╔════╝██╔══██╗
+        -- ██║  ██║██████╔╝███████╗██╔██╗ ██║ ╚████╔╝ ██║  ██║█████╗  ██████╔╝
+        -- ██║  ██║██╔══██╗╚════██║██║╚██╗██║  ╚██╔╝  ██║  ██║██╔══╝  ██╔══██╗
+        -- ██████╔╝██║  ██║███████║██║ ╚████║   ██║   ██████╔╝███████╗██║  ██║
+        -- ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚═════╝ ╚══════╝╚═╝  ╚═╝
+
+
+fx_version 'cerulean'  game 'gta5'
+author 'DrSnyder'  name "ers_integration"  version '1.4'
 description 'ERS Integration for QB-Core & QBOX using qbx-radialmenu/qb-radailmenu + ps-dispatch'
-version '1.3'
+repository 'https://github.com/DrSnyder86/ERS-Integration'
+license ''
 
 shared_script 'config.lua'
 
 client_scripts {
     '@ox_lib/init.lua',
-    'client/*.lua'
+    'client/*.lua',
 }
 
 server_scripts {
     '@qb-core/shared/locale.lua',
-    'server/*.lua'
+    'server/*.lua',
 }
 
 dependencies {
     'qb-core',
     'ps-dispatch',
 }
-
-
 
