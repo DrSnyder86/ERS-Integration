493e3688791cb6aac64ff5a6be343a34272ea2ad
DrSnyder <drsnyder86@gmail.com>
2026-05-19 12:01:57 -0400

    Update duty locations and configurations for services

--- config/ers_targets_config.lua
+++ config/ers_targets_config.lua
@@ -8,34 +8,35 @@ Config = Config or {}
 -- Ox_target and Qb-target duty locations
 Config.DutyPoints = {
     police = {
-        vec3(441.18, -981.97, 30.69),   --MRPD
-        vec3(1854.03, 3687.2, 34.22),   --SANDY
-		vec3(-447.3, 6013.3, 31.72),
-		vec3(362.72, -1590.5, 29.29),
-		vec3(483.99, 2652.12, 43.17),
-		vec3(1536.58, 795.69, 77.65),
-		--vec3(-3149.23, 1135.89, 21.07),
-		vec3(1659.65, 4793.77, 42.26),
-		vec3(633.9, 6.78, 82.63),
-		vec3(2573.07, 5055.07, 44.64),
-		vec3(831.69, -1293.64, 26.72),
-		vec3(-1097.17, -841.96, 19.32),
+        vec3(441.689606,-982.033081,30.836691),   -- Mission Row
+        vec3(1853.898926,3687.701660,34.368851),   -- Sandy Sheriff
+		vec3(-447.184235,6012.980957,32.419365), -- Paleto Sheriff
+		vec3(362.951691,-1590.943604,29.398693), -- Davis Sheriff
+		vec3(484.157379, 2651.660156, 42.979469), -- Harmony BCSO
+		vec3(1537.086304,795.364746,77.799271), -- SAHP LS Freeway
+		vec3(1659.286987,4793.820312,42.326149), -- Grapeseed Sheriff
+		vec3(2572.544678,5054.394043,44.642319), -- Blaine County SAHP
+		vec3(831.680054,-1294.253906,26.875271), -- SAHP Popular St
+		vec3(-1096.752930,-841.261475,19.343437), -- Vespucci PD
+        vec3(-755.046448, -1521.858154, 4.946587), -- LSSD La Puerta Heliport
     },
     ambulance = {
-        vec3(441.18, -981.97, 30.69),   --MRPD
-        vec3(311.18, -599.25, 43.29),
-        vec3(-254.88, 6324.5, 32.58),
+        vec3(307.449036,-595.337158,43.123158), -- Pillbox Medical
+        vec3(1833.285400,3676.097412,34.274117), -- Sandy Medical
+        vec3(-251.889236,6334.766113,32.445705), -- Paleto Medical
     },
     fire = {
-        vec3(441.18, -981.97, 30.69),   --MRPD
-        vec3(311.18, -599.25, 43.29),
-        vec3(-254.88, 6324.5, 32.58),
+        vec3(197.595993, -1650.366699, 28.904411), -- Davis Fire
+        vec3(-1681.793945, 53.475166, 64.089745), -- Richman Fire
+        vec3(-1233.390259,-1396.312866,4.278352), -- Vespucci Fire
+        vec3(1773.531128, 4604.865234, 37.632195), -- Grapeseed Fire
+        vec3(-375.695068,6117.116211,31.429543), -- Paleto FD
     },
     tow = {
-        vec3(441.18, -981.97, 30.69),   --MRPD
-        vec3(471.39, -1311.03, 29.21),
-        vec3(210.0, -1410.0, 30.5),
-        vec3(190.0, -1390.0, 30.5),
+        vec3(471.466644, -1310.989868, 29.237516), -- Hayes Auto - Little Bighorn
+        vec3(891.851440, 3604.529053, 33.305969), -- Sandy Customs
+        vec3(1186.479614, 2637.883789, 38.232563), -- Harmony Repair
+        vec3(100.728455, 6620.212402, 32.266003), -- Paleto Customs
     }
 }
 
@@ -48,7 +49,7 @@ Config.DutyConfig = {
     fire = { icon = "fa-solid fa-fire", label = "ERS Fire Duty", event = "ers:server:ToggleFireShift" },
     tow = { icon = "fa-solid fa-car-burst", label = "ERS Tow Duty", event = "ers:server:ToggleTowShift" },
 }
--- Qb-target
+-- -- Qb-target
 -- Config.DutyConfig = {
 --     police = { icon = "user-shield", label = "ERS Police Duty", event = "ers:server:TogglePoliceShift" },
 --     ambulance = { icon = "briefcase-medical", label = "ERS Ambulance Duty", event = "ers:server:ToggleAmbulanceShift" },
